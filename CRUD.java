4030f044f075830fa18e8738460ef182502bdae4
RSalcido2019 <ricardo.salcido@outlook.com>
2022-05-22 19:09:07 -0500

    Employee class

---
+++ CRUD.java
@@ -0,0 +1,13 @@
+import java.security.SecureRandom;
+
+public class Employee {
+    private int empno;
+    private String ename;
+    private int salary;
+
+    public Employee(int empno, String ename, int salary){
+        this.empno = empno;
+        this.ename = ename;
+        this.salary = salary;
+    }
+}
